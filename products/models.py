3c5462bc708560dc5078146a6ade77a049399a0a
Natalia Gorbashova <natasha_gorbasho@mail.ru>
2023-05-15 18:59:18 +1200

    admin changes

--- products/models.py
+++ products/models.py
@@ -5,6 +5,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
@@ -15,6 +19,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
     def __str__(self):
         return f'Продукт: {self.name}| Категория: {self.category.name}'
@@ -34,7 +42,7 @@ class Basket(models.Model):
 
     objects = BasketQuerySet.as_manager()
     def __str__(self):
-        return f'Корзина для {self.user.name} | Продукт: {self.product.name}'
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
 
     def sum(self):
         return self.product.price * self.quantity
